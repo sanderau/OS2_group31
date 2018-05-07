30aaf60b490e4a192aea59db5a2165cfe513bce0
Austin Sanders <sanderau@os2.engr.oregonstate.edu>
2018-05-07 11:51:14 -0700

    adding and cleaning up the beginning of the sstf sched

--- docs/HW2/source/sstf-iosched.c
+++ docs/HW2/source/sstf-iosched.c
@@ -5,20 +5,29 @@
 #include <linux/bio.h>
 #include <linux/module.h>
 #include <linux/slab.h>
-#include <linux/init.h>
+#include <linux/init.h> /* Module_init and module_exit macros*/
 #include <linux/list.h>
 #include <linux/kernel.h>
 
+/*Varaible Definitions*/
+#define DRV_AUTHORS "Austin Sanders, Zachary Tusing, Kevin Talik"
+#define DRV_DESC "An SSTF (Shortest Seek Time First) I/O scheduler"
+
 /* References */
+
+	/*
+	 * [0]: https://www.tldp.org/LDP/lkmpg/2.4/html/book1.htm - module programming
+	 * [1]: 
+	 */
 
 struct sstf_data {
 	struct list_head queue;
 }
 
 
-module_init(sstf_init);
-module_exit(sstf_exit);
+module_init(sstf_init); //entry point for the kernel to execute the driver at boot time
+module_exit(sstf_exit); //exit point for the kernel to exit the driver
 
-MODULE_AUTHOR("Austin Sanders");
-MODULE_LICENSE("GPL");
-MODULE_DESCRIPTION("SSTF IO Scheduler");
+MODULE_AUTHOR(DRV_AUTHORS);
+MODULE_LICENSE("GPL"); /* General public license */
+MODULE_DESCRIPTION(DRV_DESC); /*Description of the Driver*/
