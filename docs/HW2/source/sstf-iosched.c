2be0030575e2b6d4d0a0e04ada3950dc54b3b583
Austin Sanders <sanderau@os2.engr.oregonstate.edu>
2018-05-07 16:19:47 -0700

    I added the initiate and exit functions for the module. Now all that is left is implementing all of the functionality for the elevator structure

--- docs/HW2/source/sstf-iosched.c
+++ docs/HW2/source/sstf-iosched.c
@@ -7,7 +7,7 @@
 #include <linux/module.h>
 #include <linux/slab.h>
 #include <linux/init.h> /* Module_init and module_exit macros*/
-#include <linux/list.h>
+#include <linux/list.h> /* list_head data structure */
 #include <linux/kernel.h>
 
 /*Varaible Definitions*/
@@ -18,28 +18,41 @@
 
 	/*
 	 * [0]: https://www.tldp.org/LDP/lkmpg/2.4/html/book1.htm - module programming
-	 * [1]: 
+	 * [1]: https://www.elixir.bootlin.com/linux/v3.6/indent/ - where I got all of my information on required libraries 
+	 * * Note: I used many pages from this website, but I just referenced the search engine that I used
+	 * [2]: 
 	 */
 
 struct sstf_data {
 	struct list_head queue;
 }
 
-/*			INIT and EXIT functions		*/
+/*		Elevator Data Structure		*/
+
+static struct elevator_type elevator_sstf = {
+	.ops = {
+
+	},
+	.elevator_name = "SSTF",
+	.elevator_owner = THIS_MODULE
+};
+
+
+/*		INIT and EXIT functions		*/
 
 /*
  * This function is what actually initializes the module. This is the function
  * called by the kernel when initialized.
  */
 static int __init sstf_init(void) {
-	//TODO: actually write the init for this module lol
+	return elv_register(&elevator_sstf);	
 }
 
 /*
  * Clean up function that is called on the modules exit in the kernel 
  */
 static void __exit sstf_exit(void) {
-	//TODO: write the cleanup function to be called on the modules exit
+	return elv_unregister(&elevator_sstf);
 }
 
 module_init(sstf_init); //entry point for the kernel to execute the driver at boot time
