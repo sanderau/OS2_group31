b3676dd9d4fded7f7df474a8348340eec43a061f
Austin Sanders <sanderau@os2.engr.oregonstate.edu>
2018-05-07 18:35:33 -0700

    init function is implemented and compiles

--- docs/HW2/source/sstf-iosched.c
+++ docs/HW2/source/sstf-iosched.c
@@ -1,14 +1,14 @@
 /* Elevator SSTF */
 /* Authors: Austin Sanders */
 
-#include <linux/blkdev.h>
+#include <linux/blkdev.h> /* Items used: request_queue, */
 #include <linux/elevator.h>
 #include <linux/bio.h>
 #include <linux/module.h>
 #include <linux/slab.h>
 #include <linux/init.h> /* Module_init and module_exit macros*/
 #include <linux/list.h> /* list_head data structure */
-#include <linux/kernel.h>
+#include <linux/kernel.h> /* */
 
 /*Varaible Definitions*/
 #define MOD_AUTHORS "Austin Sanders, Zachary Tusing, Kevin Talik"
@@ -23,18 +23,63 @@
 	 * [2]: 
 	 */
 
+/* List that will contain the sector requests */
+
 struct sstf_data {
 	struct list_head queue;
+};
+
+/*	Functions	*/
+
+static int sstf_init_queue(struct request_queue *rq, struct elevator_type *et)
+{
+	struct sstf_data *sd;
+	struct elevator_queue *eq;
+
+	eq = elevator_alloc(rq, et);
+
+	if(!eq) {
+		/*If we cannot allocate memory, return the correct error message */		
+		return ENOMEM; 
+	}
+
+	sd = kmalloc_node(sizeof(*sd), GFP_KERNEL, rq->node);
+	if(!sd) {
+		kobject_put(&eq->kobj);
+		return ENOMEM; /* if we can not allocate memory for the queue, sent appropriate error code */
+	}
+
+	eq->elevator_data=sd;
+
+
+	INIT_LIST_HEAD(&sd->queue);
+
+	spin_lock_irq(rq->queue_lock);
+	rq->elevator = eq;
+	spin_unlock_irq(rq->queue_lock);
+
+	printk(KERN_DEBUG "SSTF: INIT queue\n");
+	return 0;
+
+
 }
+
 
 /*		Elevator Data Structure		*/
 
 static struct elevator_type elevator_sstf = {
+	/*
+	 * ops - the operations of the newly defined elevator type
+	 * elevator_name - name of the newly written elevator
+	 * elevator_owner - module that owns this elevator. 
+	 * * Prevents premature termination by kernel.
+	 */
 	.ops = {
+		.elevator_init_fn 	= sstf_init_queue, /*initialize the elevator */
 
 	},
 	.elevator_name = "SSTF",
-	.elevator_owner = THIS_MODULE
+	.elevator_owner = THIS_MODULE,
 };
 
 
@@ -45,7 +90,8 @@ static struct elevator_type elevator_sstf = {
  * called by the kernel when initialized.
  */
 static int __init sstf_init(void) {
-	return elv_register(&elevator_sstf);	
+	/* */
+	return elv_register(&elevator_sstf); 	
 }
 
 /*
