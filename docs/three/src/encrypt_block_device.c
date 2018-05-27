bf68698f550ceb85223c2a73275d08c0ed85d09f
tusingz <zttusing@protonmail.ch>
2018-05-26 22:23:38 -0700

    Adding this file so austin can see it. Still working on modifing it so it accomplishes what we need. Not ready to compile. Still applying the block device and checking yocto systems to make sure it fits between these requirements

--- docs/three/src/encrypt_block_device.c
+++ docs/three/src/encrypt_block_device.c
@@ -22,23 +22,130 @@ References:
 MODULE_LICENSE("Dual BSD/GPL");
 
 
-char *myKey = NULL;
+static struct request_queue *Queue;
 
-module_param(myKey, charp, 0000);
-MODULE_PARM_DESC(mystring, "An optional key for the user to provide");
+/*
+ * The internal representation of our device.
+ */
+static struct sbd_device {
+    unsigned long size;
+    u8 *data;
+    struct gendisk *gd;
+} Device;
 
-static int hello_init(void)
+
+static void sbd_request(request_queue_t *q)
 {
-	//void *kmalloc(size_t size, int flags);
-	return 0;
+    struct request *req;
+
+    while ((req = elv_next_request(q)) != NULL) {
+	if (! blk_fs_request(req)) {
+	    printk (KERN_NOTICE "Skip non-CMD request\n");
+	    end_request(req, 0);
+	    continue;
+	}
+	sbd_transfer(&Device, req->sector, req->current_nr_sectors,
+			req->buffer, rq_data_dir(req));
+	end_request(req, 1);
+    }
 }
 
-static void hello_exit(void)
-{
 
-	//kfree(const void* objp);
-	return 0;
+
+/*
+ * Ioctl.
+ */
+int sbd_ioctl (struct inode *inode, struct file *filp,
+                 unsigned int cmd, unsigned long arg)
+{
+	long size;
+	struct hd_geometry geo;
+
+	switch(cmd) {
+		//this should be where encryption will go I might need to write another encrypt
+		//https://kernel.readthedocs.io/en/sphinx-samples/crypto-API.html
+	    case HDIO_GETGEO:
+		size = Device.size*(hardsect_size/KERNEL_SECTOR_SIZE);
+		geo.cylinders = (size & ~0x3f) >> 6;
+		geo.heads = 4;
+		geo.sectors = 16;
+		geo.start = 4;
+		if (copy_to_user((void *) arg, &geo, sizeof(geo)))
+			return -EFAULT;
+		return 0;
+    }
+
+    return -ENOTTY; /* unknown command */
 }
 
-module_init(hello_init);
-module_exit(hello_exit);
+
+
+
+/*
+ * The device operations structure.
+ */
+static struct block_device_operations sbd_ops = {
+    .owner           = THIS_MODULE,
+    .ioctl	     = sbd_ioctl
+};
+
+static int __init sbd_init(void)
+{
+/*
+ * Set up our internal device.
+ */
+    Device.size = nsectors*hardsect_size;
+
+    Device.data = kmalloc(Device.size);
+    if (Device.data == NULL)
+	return -ENOMEM;
+/*
+ * Get a request queue.
+ */
+    Queue = blk_init_queue(sbd_request, &Device.lock);
+    if (Queue == NULL)
+	    goto out;
+    blk_queue_hardsect_size(Queue, hardsect_size);
+/*
+ * Get registered.
+ */
+    major_num = register_blkdev(major_num, "sbd");
+    if (major_num <= 0) {
+	printk(KERN_WARNING "sbd: unable to get major number\n");
+	goto out;
+    }
+/*
+ * And the gendisk structure.
+ */
+    Device.gd = alloc_disk(16);
+    if (! Device.gd)
+	goto out_unregister;
+    Device.gd->major = major_num;
+    Device.gd->first_minor = 0;
+    Device.gd->fops = &sbd_ops;
+    Device.gd->private_data = &Device;
+    strcpy (Device.gd->disk_name, "sbd0");
+    set_capacity(Device.gd, nsectors*(hardsect_size/KERNEL_SECTOR_SIZE));
+    Device.gd->queue = Queue;
+    add_disk(Device.gd);
+
+    return 0;
+
+  out_unregister:
+    unregister_blkdev(major_num, "sbd");
+  out:
+    vfree(Device.data);
+    return -ENOMEM;
+}
+
+static void __exit sbd_exit(void)
+{
+    del_gendisk(Device.gd);
+    put_disk(Device.gd);
+    unregister_blkdev(major_num, "sbd");
+    blk_cleanup_queue(Queue);
+    vfree(Device.data);
+}
+	
+module_init(sbd_init);
+module_exit(sbd_exit);
